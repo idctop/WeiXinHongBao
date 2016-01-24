f6bc314c575c7e7356e4622dcc9a6e9a7cbe447d
Khoo <idctop@163.com>
2016-01-24 15:43:27 +0800

    测试本地GIT调试
    测试用

---
+++ app/src/main/java/xyz/monkeytong/hongbao/activities/MainActivity.java
@@ -0,0 +1,125 @@
+package xyz.monkeytong.hongbao.activities;
+
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.view.View;
+import android.view.Window;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import xyz.monkeytong.hongbao.R;
+import xyz.monkeytong.hongbao.utils.UpdateTask;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private final Intent mAccessibleIntent =
+            new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+
+    private Button switchPlugin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switchPlugin = (Button) findViewById(R.id.button_accessible);
+
+        handleMIUIStatusBar();
+        updateServiceStatus();
+
+        explicitlyLoadPreferences();
+    }
+
+    private void explicitlyLoadPreferences() {
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+    }
+
+    /**
+     * 适配MIUI沉浸状态栏
+     */
+    private void handleMIUIStatusBar() {
+        Window window = getWindow();
+
+        Class clazz = window.getClass();
+        try {
+            int tranceFlag = 0;
+            Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_TRANSPARENT");
+            tranceFlag = field.getInt(layoutParams);
+
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            extraFlagField.invoke(window, tranceFlag, tranceFlag);
+
+            ImageView placeholder = (ImageView) findViewById(R.id.main_actiob_bar_placeholder);
+            int placeholderHeight = getStatusBarHeight();
+            placeholder.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, placeholderHeight));
+        } catch (Exception e) {
+            // 考虑到大多数非MIUI ROM都会打印出错误栈,不太优雅,而且一点卵用也没有,于是删了
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateServiceStatus();
+
+        // Check for update
+        new UpdateTask(this, false).update();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void updateServiceStatus() {
+        boolean serviceEnabled = false;
+
+        AccessibilityManager accessibilityManager =
+                (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> accessibilityServices =
+                accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        for (AccessibilityServiceInfo info : accessibilityServices) {
+            if (info.getId().equals(getPackageName() + "/.HongbaoService")) {
+                serviceEnabled = true;
+                break;
+            }
+        }
+
+        if (serviceEnabled) {
+            switchPlugin.setText(R.string.service_off);
+        } else {
+            switchPlugin.setText(R.string.service_on);
+        }
+    }
+
+    public void onButtonClicked(View view) {
+        startActivity(mAccessibleIntent);
+    }
+
+    public void openGithub(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.hailunpiano.com"));
+        startActivity(browserIntent);
+    }
+
+    public void openSettings(View view) {
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        startActivity(settingsIntent);
+    }
+
+    public int getStatusBarHeight() {
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) return getResources().getDimensionPixelSize(resourceId);
+        return 0;
+    }
+}
